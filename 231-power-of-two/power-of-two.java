f74584a93d6fc8423e761053a384f000ea65277d
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-02-23 17:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (53.41%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        long i=1;
-        while (i<n){
-            i=i*2;
-        }
+        // long i=1;
+        // while (i<n){
+        //     i=i*2;
+        // }
 
-        return i==n ? true:false;
+        // return i==n ? true:false;
+        if(n<=0){
+            return false;
+        }
+        return (n & (n-1)) == 0;
     }
 }
