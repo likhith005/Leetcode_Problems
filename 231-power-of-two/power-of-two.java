4070c7365cb98e959a79ef0da80cfdb95d9d057c
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-02-23 17:39:39 +0530

    Time: 1 ms (46.58%) | Memory: 40.5 MB (81.73%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -12,12 +12,18 @@ class Solution {
         // }
         // return (n & (n-1)) == 0;
 
+        // if(n<=0){
+        //     return false;
+        // }
+        // if(n==1){
+        //     return true;
+        // }
+        // return n%2==0 && isPowerOfTwo(n/2);
+
         if(n<=0){
             return false;
         }
-        if(n==1){
-            return true;
-        }
-        return n%2==0 && isPowerOfTwo(n/2);
+
+        return Integer.bitCount(n)==1;
     }
 }
