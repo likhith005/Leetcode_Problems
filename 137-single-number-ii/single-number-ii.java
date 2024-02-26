af00ce2c78d85b31eed14200a06ef173e3a81b49
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-02-26 22:25:58 +0530

    Time: 1 ms (88.56%) | Memory: 45.3 MB (56.53%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,20 +1,27 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-        for(int k = 0; k <= 31; k++){
-            int temp = (1<<k);
-            int countOne = 0;
+        // int result = 0;
+        // for(int k = 0; k <= 31; k++){
+        //     int temp = (1<<k);
+        //     int countOne = 0;
 
-            for(int num : nums){
-                if((num & temp) != 0){
-                countOne++;
-                }
-            }
+        //     for(int num : nums){
+        //         if((num & temp) != 0){
+        //         countOne++;
+        //         }
+        //     }
 
-            if((countOne % 3) != 0){
-                result |= temp;
-            }
+        //     if((countOne % 3) != 0){
+        //         result |= temp;
+        //     }
+        // }
+        // return result;
+
+        int seenOnce = 0, seenTwice = 0;
+        for (int num : nums) {
+            seenOnce = (seenOnce ^ num) & (~seenTwice);
+            seenTwice = (seenTwice ^ num) & (~seenOnce);
         }
-        return result;
+        return seenOnce;
     }
 }
