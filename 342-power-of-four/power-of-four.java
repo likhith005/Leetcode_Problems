4ce20541be76f7bd912062bf864733f68e55695c
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-02-26 13:19:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (66.24%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -3,29 +3,36 @@ class Solution {
         // if (n <= 0) {
         //     return false;
         // }
-        
         // // Check if n is a power of two
         // if ((n & (n - 1)) != 0) {
         //     return false;
         // }
-        
         // // Count the number of trailing zeroes
         // int countZeros = 0;
         // while (n > 1) {
         //     n >>= 1;
         //     countZeros++;
         // }
-        
         // // Check if the number of trailing zeroes is even
         // return (countZeros % 2 == 0);
+
+
+
+        // if(n<=0){
+        //     return false;
+        // }
+        // while((n%4)==0){
+        //     n /= 4;
+        // }
+        // return (n==1) ? true : false;
+
+
 
         if(n<=0){
             return false;
         }
-        while((n%4)==0){
-            n /= 4;
-        }
-        return (n==1) ? true : false;
+        int x = (int) (Math.log(n)/Math.log(4));
+        return Math.pow(4,x)==n;
     }
 }
     
