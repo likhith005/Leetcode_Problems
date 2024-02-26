fffaa538cef1697e08459f3159ed10be67df0651
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-02-26 15:45:59 +0530

    Time: 8 ms (92.87%) | Memory: 43.4 MB (99.17%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -9,9 +9,11 @@ class Solution {
         // return (n==1);
 
         
-        if (n <= 0) {
-            return false;
-        }
-        return Math.log10(n) / Math.log10(3) % 1 == 0;
+        // if (n <= 0) {
+        //     return false;
+        // }
+        // return Math.log10(n) / Math.log10(3) % 1 == 0;
+
+        return n > 0 && 1162261467 % n == 0;
     }
 }
