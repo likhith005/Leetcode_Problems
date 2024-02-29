dec6caf1460e2b5a78c91249e4fa29f2ad038ad7
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-02-29 21:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (61.71%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,23 +1,31 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
-        int flip = 0;
-        while(a!=0 || b!=0 || c!=0){
-            if((c&1)==1){
-                if((a&1)==0 && (b&1)==0){
-                    flip++;
-                }
-            }else{
-                if((c&1)==0){
-                    if((a&1)==1)
-                        flip++;
-                    if((b&1)==1)
-                        flip++;
-                }
-            }
-            a >>=1;
-            b >>=1;
-            c >>=1;
-        }
-        return flip;
+        // int flip = 0;
+        // while(a!=0 || b!=0 || c!=0){
+        //     if((c&1)==1){
+        //         if((a&1)==0 && (b&1)==0){
+        //             flip++;
+        //         }
+        //     }else{
+        //         if((c&1)==0){
+        //             if((a&1)==1)
+        //                 flip++;
+        //             if((b&1)==1)
+        //                 flip++;
+        //         }
+        //     }
+        //     a >>=1;
+        //     b >>=1;
+        //     c >>=1;
+        // }
+        // return flip;
+
+        int result = ((a|b)^c);
+       
+        int result1 = (a & b);
+       
+        int result2 = (result1 & result);
+        
+        return Integer.bitCount(result) + Integer.bitCount(result2);
     }
 }
